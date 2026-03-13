60d62bf279206511d0fcfd698791efaab306c174
atifdire-boop <atifdire@gmail.com>
2026-03-13 09:07:20 +0500

    Create tools/__init__.py with tools list
    Initialized tools module with 20 tools listed.

---
+++ tools/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+DIRE-X Tools Module
+All 20 tools included
+"""
+
+__all__ = [
+    'dire_info',      # Tool 1
+    'dire_scan',      # Tool 2
+    'dire_brute',     # Tool 3
+    'dire_sql',       # Tool 4
+    'dire_xss',       # Tool 5
+    'dire_phish',     # Tool 6
+    'dire_ddos',      # Tool 7
+    'dire_anon',      # Tool 8
+    'dire_wifi',      # Tool 9
+    'dire_mal',       # Tool 10
+    'dire_crypto',    # Tool 11
+    'dire_osint',     # Tool 12
+    'dire_exploit',   # Tool 13
+    'dire_web',       # Tool 14
+    'dire_social',    # Tool 15
+    'dire_cam',       # Tool 16
+    'dire_report',    # Tool 17
+    'dire_update',    # Tool 18
+    'dire_backup',    # Tool 19
+    'dire_help'       # Tool 20
+]
